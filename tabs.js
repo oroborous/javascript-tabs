fced73544a4b3474c84841bb3e37c3c8db80b6b3
Stacy Read <stacy.read.007@gmail.com>
2020-02-29 10:47:54 -0600

    Added output for values of checked boxes

--- tabs.js
+++ tabs.js
@@ -1,51 +1,59 @@
 // Run the init function when page finishes loading
 $(init);
 
-function init()
-{
-	// Get all anchors that are the clickable tabs
-	var tabAnchors = $(".tabTop");
-	
-	// Tell all tabs to run doTabClick when they are clicked
-	tabAnchors.click(doTabClick);
-	
-	// Programmatically perform a "click" on the first tab
-	tabAnchors.first().click();
+function init() {
+    // Get all anchors that are the clickable tabs
+    var tabAnchors = $(".tabTop");
 
-	// Not part of the tabs, but shows how to count checkboxes
-	$("button").click(orderSamples);
+    // Tell all tabs to run doTabClick when they are clicked
+    tabAnchors.click(doTabClick);
+
+    // Programmatically perform a "click" on the first tab
+    tabAnchors.first().click();
+
+    // Not part of the tabs, but shows how to count checkboxes
+    $("button").click(orderSamples);
 }
 
-function orderSamples()
-{
-	// jQuery, get me all the inputs where the name is sample
-	// that are checked, and count how many
-	var numSamples = $("input[name='sample']:checked").length;
+function orderSamples() {
+    // jQuery, get me all the inputs where the name is sample
+    // that are checked
+    var checkedBoxes = $("input[name='sample']:checked");
 
-	// Output the number of samples asked for
-	$("#output").text("You asked for " + numSamples + " samples");
+    // Output the number of samples asked for
+    $("#sampleCount").text("You asked for " + checkedBoxes.length + " samples");
+
+    // Create an empty array to hold requested soap names (as strings)
+    var soaps = [];
+
+    // Push the value attribute of each checked box into the array
+    checkedBoxes.each(function () {
+        soaps.push(this.value);
+    });
+
+    // Concatenate the strings in the array with "comma space" in between them
+    $("#sampleNames").text(soaps.join(", "));
 }
 
-function doTabClick()
-{
-	// Get all of the divs that contain the tabs' contents
-	var tabContainers = $(".tabBody");
-	
-	// Hide them all
-	tabContainers.hide();
-	
-	// The hash is the part after the # in the href attribute
-	tabContainers.filter(this.hash).show();
-	
-	// Get all anchors that are the clickable tabs
-	var tabAnchors = $(".tabTop");
-	
-	// Remove the "selected" class from all tabs
-	tabAnchors.removeClass("selected");
-	
-	// Add the "selected" class to just the tab that was clicked on
-	$(this).addClass("selected");
-	
-	// Don't try to follow the link (because we're using <a> tags)
-	return false;	
+function doTabClick() {
+    // Get all of the divs that contain the tabs' contents
+    var tabContainers = $(".tabBody");
+
+    // Hide them all
+    tabContainers.hide();
+
+    // The hash is the part after the # in the href attribute
+    tabContainers.filter(this.hash).show();
+
+    // Get all anchors that are the clickable tabs
+    var tabAnchors = $(".tabTop");
+
+    // Remove the "selected" class from all tabs
+    tabAnchors.removeClass("selected");
+
+    // Add the "selected" class to just the tab that was clicked on
+    $(this).addClass("selected");
+
+    // Don't try to follow the link (because we're using <a> tags)
+    return false;
 }
