d5be5ddb546cd43c0d21d0238b98f54a79f163f4
Stacy Read <stacy.read.007@gmail.com>
2020-11-01 13:27:12 -0600

    Revert "Update README.md"
    This reverts commit a17f0e9b9acdc24311e9357f8f5a2ccf77c7ee86.
    
    # Conflicts:
    #	README.md

--- README.md
+++
@@ -1,5 +0,0 @@
-# javascript-tabs
-
-Demonstration of constructing tab navigation using CSS styles and jQuery.
-
-Tab one demonstrates how to count the number of checked checkboxes. It also shows how to output the values of the ones that were checked.
--- tabs.js
+++ tabs.js
@@ -1,59 +1,24 @@
-// Run the init function when page finishes loading
-$(init);
+$(document).ready(
+    function () {
+        // event handlers
+        $("#tabs a").click(showTab);
+        $("#animateButton").click(animateSandwich);
 
-function init() {
-    // Get all anchors that are the clickable tabs
-    var tabAnchors = $(".tabTop");
 
-    // Tell all tabs to run doTabClick when they are clicked
-    tabAnchors.click(doTabClick);
+        // other functions
+        function showTab(event) {
+            event.preventDefault();
+            $(this).tab("show");
+        }
 
-    // Programmatically perform a "click" on the first tab
-    tabAnchors.first().click();
-
-    // Not part of the tabs, but shows how to count checkboxes
-    $("button").click(orderSamples);
-}
-
-function orderSamples() {
-    // jQuery, get me all the inputs where the name is sample
-    // that are checked
-    var checkedBoxes = $("input[name='sample']:checked");
-
-    // Output the number of samples asked for
-    $("#sampleCount").text("You asked for " + checkedBoxes.length + " samples");
-
-    // Create an empty array to hold requested soap names (as strings)
-    var soaps = [];
-
-    // Push the value attribute of each checked box into the array
-    checkedBoxes.each(function () {
-        soaps.push(this.value);
+        function animateSandwich() {
+            // Fade to 0% opacity over 750 milliseconds
+            // Wait 250 milliseconds
+            // Fade to 100% opacity over 750 milliseconds
+            // Hide the sandwich by shrinking its height over 750 ms
+            // Wait 250 ms
+            // Show the sandwich by increasing its height over 750 ms
+            $("#sandwich").fadeTo(750, 0).delay(250).fadeTo(750, 1)
+                .slideUp(750).delay(250).slideDown(750);
+        }
     });
-
-    // Concatenate the strings in the array with "comma space" in between them
-    $("#sampleNames").text(soaps.join(", "));
-}
-
-function doTabClick() {
-    // Get all of the divs that contain the tabs' contents
-    var tabContainers = $(".tabBody");
-
-    // Hide them all
-    tabContainers.hide();
-
-    // The hash is the part after the # in the href attribute
-    tabContainers.filter(this.hash).show();
-
-    // Get all anchors that are the clickable tabs
-    var tabAnchors = $(".tabTop");
-
-    // Remove the "selected" class from all tabs
-    tabAnchors.removeClass("selected");
-
-    // Add the "selected" class to just the tab that was clicked on
-    $(this).addClass("selected");
-
-    // Don't try to follow the link (because we're using <a> tags)
-    return false;
-}
